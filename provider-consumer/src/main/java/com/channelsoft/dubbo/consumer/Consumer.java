0a2762dbca224d0285eba641bd007642cfe52324
sicwen <1224260992@qq.com>
2019-05-12 00:00:58 +0800

    add multi thread getmonitor module

--- provider-consumer/src/main/java/com/channelsoft/dubbo/consumer/Consumer.java
+++ provider-consumer/src/main/java/com/channelsoft/dubbo/consumer/Consumer.java
@@ -6,11 +6,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Consumer {
     @Test
-    public void testConsumer(){
+    public void testConsumer() throws InterruptedException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"consumer.xml"});
         context.start();
         DemoService demoService = (DemoService)context.getBean("demoService"); // 获取远程服务代理
-        String hello = demoService.sayHello("world"); // 执行远程方法
-        System.out.println( hello ); // 显示调用结果
+
+        for (int i = 0; i < 2; i++) {
+            String hello = demoService.sayHello("world"); // 执行远程方法
+            System.out.println( hello ); // 显示调用结果
+            Thread.sleep(2000);
+        }
+
+        Thread.sleep(10000);
+
     }
 }
